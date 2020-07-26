e2c6ff5e27dfd34c381600b1a1457d3ff8d9e6cb
shahalam13 <shahalam7303@gmail.com>
2020-07-26 14:49:04 +0600

    status button update

--- src/app/pages/tables/ngx-datatable/ngx-datatable.component.ts
+++ src/app/pages/tables/ngx-datatable/ngx-datatable.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-
+import { ToastrService } from "ngx-toastr";
 
 
 @Component({
@@ -51,7 +51,20 @@ export class NgxDatatableComponent implements OnInit {
   ];
 
 
-  constructor() {
+
+  // constructor() {
+  //   this.temp = this.rows.map((prop,key)=>{
+  //     return {
+  //       ...prop,
+  //       id: key
+  //     };
+
+  //   });
+  // }
+
+
+  constructor(private toastr: ToastrService) {
+
     this.temp = this.rows.map((prop,key)=>{
       return {
         ...prop,
@@ -59,6 +72,45 @@ export class NgxDatatableComponent implements OnInit {
       };
 
     });
+  }
+  showNotification(from, align) {
+    const color = Math.floor(Math.random() * 5 + 1);
+
+    switch (color) {
+      case 1:
+   
+        this.toastr.success(
+          '<span class="now-ui-icons ui-1_check"></span> Success!  <b>Status</b> - has been update successfully.',
+          "",
+          {
+            timeOut: 8000,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-success alert-with-icon",
+            positionClass: "toast-" + from + "-" + align
+          }
+        );
+        break;
+
+      case 2:
+        this.toastr.success(
+          '<span class="now-ui-icons ui-1_check"></span> Success!  <b>Status</b> - has been update successfully.',
+          "",
+          {
+            timeOut: 8000,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-success alert-with-icon",
+            positionClass: "toast-" + from + "-" + align
+          }
+        );
+        break;
+
+
+
+      default:
+        break;
+    }
   }
   entriesChange($event){
     this.entries = $event.target.value;
